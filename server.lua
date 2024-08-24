8226527cf5454a1ab203d9f4fb60f5dfde7aeddc
cobalt61 <aramzar@icloud.com>
2024-08-24 11:40:30 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,1 +1,12 @@
-
+local lighting = game.Lighting
+local id = "rbxassetid://9411523156"
+local sky = lighting.Sky
+sky.SkyboxBk = id
+sky.SkyboxDn = id
+sky.SkyboxFt = id
+sky.SkyboxLf = id
+sky.SkyboxRt = id
+sky.SkyboxUp = id
+sky.MoonTextureId = id
+sky.SunTextureId = id
+-- you will never figure out what this does >:)
